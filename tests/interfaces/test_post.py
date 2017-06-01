c9afe11825c159201f3cb7972e572a8c58964643
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-01 23:20:04 +0200

    integrate tests for the multiple row insert

--- tests/interfaces/test_post.py
+++ tests/interfaces/test_post.py
@@ -4,24 +4,27 @@ import requests
 
 BASE_URL = 'http://localhost:8080'
 
+DATA = {
+    "messages": [
+        {"message": "hello world"},
+        {"message": "bonjour tout le monde"},
+        {"message": "da jia hao"}
+    ]
+}
+
 def test_post_insert_transaction():
-    r = requests.post(BASE_URL + '/insert-transaction')
+    r = requests.post(
+        BASE_URL + '/insert-transaction',
+        headers={'Content-type': 'application/json'},
+        data=json.dumps(DATA)
+    )
     assert r.status_code == 200
 
 def test_post_multiple_inserts():
-
-    data = {
-        "messages": [
-            {"message": "hello world"},
-            {"message": "bonjour tout le monde"},
-            {"message": "da jia hao"}
-        ]
-    }
-
     r = requests.post(
         BASE_URL + '/multiple-inserts',
         headers={'Content-type': 'application/json'},
-        data=json.dumps(data)
+        data=json.dumps(DATA)
     )
 
     assert r.status_code == 200
