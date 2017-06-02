83b050134dca58f21a7707e798bbe936d224c73c
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-02 16:00:21 +0200

    integrate locust tests for all cases

--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,43 @@
+import json
+
 from locust import HttpLocust
 from locust import TaskSet
 from locust import task
 
 class UserBehavior(TaskSet):
 
+    @task(0)
+    def post_copy(self):
+
+        items = [{"message": "hello world"}]
+        data = {"messages": items * 100}
+
+        self.client.post(
+            "/copy",
+            json.dumps(data),
+        )
+
+    @task(0)
+    def post_insert_transaction(self):
+
+        items = [{"message": "hello world"}]
+        data = {"messages": items * 100}
+
+        self.client.post(
+            "/insert-transaction",
+            json.dumps(data),
+        )
+
     @task(10)
-    def ping(self):
-        self.client.post("/copy")
+    def post_multiple_inserts(self):
+
+        items = [{"message": "hello world"}]
+        data = {"messages": items * 100}
+
+        self.client.post(
+            "/multiple-inserts",
+            json.dumps(data),
+        )
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
